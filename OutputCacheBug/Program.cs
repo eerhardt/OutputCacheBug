a3619ff0133bdfddde9ecb06deb7b5de1e5b4ea3
Eric Erhardt <eric.erhardt@microsoft.com>
2023-08-31 09:04:26 -0500

    Add project files.

---
+++ OutputCacheBug/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.OutputCaching;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddStackExchangeRedisOutputCache(o =>
+{
+    o.Configuration = "localhost:6379";
+});
+
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(builder =>
+        builder.Expire(TimeSpan.FromSeconds(3)));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseOutputCache();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", [OutputCache] () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
